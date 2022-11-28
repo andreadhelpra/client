30d1564cc3b17ac56ac3054252b63264479414d3
andreadhelpra <andrea.dhelpra@gmail.com>
2022-11-28 13:43:08 +0000

    add search bar

--- src/IdoList.js
+++ src/IdoList.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
-import "./IdoList.css";
-import purple_sphere from "./images/purple-sphere.png"
 import transparent_sphere from "./images/transparent_sphere.png"
-
+import {CiSearch} from "react-icons/ci";
 
 function IdoList() {
     const [data, setData] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredData, setFilteredData] = useState([]);
+
+
+    const handleChange = (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+    };
 
     const fetchData = () => {
-        fetch(`http://localhost:9000/`)
-        //fetch(`https://ad900.brighton.domains/api`)
+        //fetch(`http://localhost:9000/`)
+        fetch(`https://ad900.brighton.domains/api`)
           .then((response) => response.json())
           .then((actualData) => {
-            console.log(actualData);
             setData(actualData);
-            console.log(data);
           })
           .catch((err) => {
             console.log(err.message);
@@ -24,11 +28,36 @@ function IdoList() {
       useEffect(() => {
         fetchData();
       }, []);
-      
-      <style>@import url('https://fonts.cdnfonts.com/css/lemonmilk');</style>  
+
+  
+      useEffect(() => {
+        setFilteredData( data.filter((ido) => {
+          return (ido.name.toLowerCase().includes(searchInput.toLowerCase()) 
+          || ido.source.toLowerCase().includes(searchInput.toLowerCase()) );
+        }, [data, searchInput]));
+      });
+
+      <style>@import url('https://fonts.cdnfonts.com/css/lemonmilk');</style>    
+
+      let searchIcon;
+
+      if(searchInput.length > 0){
+        searchIcon = <span className="icon"></span>
+      } else {
+        searchIcon = <span className="icon"><CiSearch/></span>
+      }
 
       return (
         <div className="list">
+          <div className="searchbar">
+          <input
+              type="search"
+              placeholder="Search IDOs"
+              onChange={handleChange}
+              value={searchInput} />
+              {searchIcon}
+          </div>
+          
           <tbody className="table">
             <tr className="headings">
               <th></th>
@@ -38,19 +67,19 @@ function IdoList() {
               <th>Private maximum allocation</th>
               <th>Source</th>
             </tr>
-            {data.map((item, index) => (
+            {filteredData.map((ido, index) => (
               <tr key={index} class="tablebody">
                  <td><img src={transparent_sphere} className="transparent_sphere"/></td>
                 <th className="smallheading">Name</th>
-                <td className="name">{item.name}</td>
+                <td className="name">{ido.name}</td>
                 <th className="smallheading">Price</th>
-                <td>{item.price}</td>
+                <td>{ido.price}</td>
                 <th className="smallheading">Public maximum allocation</th>
-                <td>{item.max_allocation_pub}</td>
+                <td>{ido.max_allocation_pub}</td>
                 <th className="smallheading">Private maximum allocation</th>
-                <td>{item.max_allocation_pvt}</td>
+                <td>{ido.max_allocation_pvt}</td>
                 <th className="smallheading">Source</th>
-                <td><a href={item.source}>{item.source}</a></td>
+                <td><a href={ido.source}>{ido.source}</a></td>
               </tr>
             ))}
           </tbody>
